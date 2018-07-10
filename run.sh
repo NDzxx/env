16d31e560d98517632e811454567f1a15380c3d3
zxx <z8965043@163.com>
2018-07-10 11:51:34 +0800

    修改为适合ubuntu

--- run.sh
+++ run.sh
@@ -6,9 +6,7 @@ if [ ${UID} == 0 ]; then
     echo "3. Python(pip)"
     echo "4. Golang"
     echo "5. Java"
-    echo "6. PHP+Apache"
-    echo "7. Tomcat"
-    echo "8. MySQL"
+    echo "6. MySQL"
     echo "第一次执行请安装基础软件包，然后选择其他操作。"
     read -p "选择你要安装的开发环境（输入前面的数字）：" i
     case "$i" in
@@ -25,47 +23,37 @@ if [ ${UID} == 0 ]; then
         read -p "选择操作（推荐直接回车安装全部）：" i
         case "$i" in
             1)
-            DEBIAN_FRONTEND=noninteractive apt-get -yq install \
-            curl
+            apt-get install -y curl
             echo "========================================================================"
             echo "curl安装完成！！"
             echo "========================================================================"
             ;;
             2)
-            DEBIAN_FRONTEND=noninteractive apt-get -yq install \
-            ca-certificates
+            apt-get install -y  ca-certificates
             echo "========================================================================"
             echo "ca-certificates安装完成！！"
             echo "========================================================================"
             ;;
             3)
-            DEBIAN_FRONTEND=noninteractive apt-get -yq install \
-            wget
+            apt-get install  -y wget
             echo "========================================================================"
             echo "wget安装完成！！"
             echo "========================================================================"
             ;;
             4)
-            DEBIAN_FRONTEND=noninteractive apt-get -yq install --no-install-recommends \
-            pwgen
+            apt-get install  -y pwgen
             echo "========================================================================"
             echo "pwgen安装完成！！"
             echo "========================================================================"
             ;;
             5)
-            DEBIAN_FRONTEND=noninteractive apt-get -yq install \
-            build-essential
+             apt-get install -y build-essential
             echo "========================================================================"
             echo "build-essential安装完成！！"
             echo "========================================================================"
             ;;
             *)
-            DEBIAN_FRONTEND=noninteractive apt-get -yq install \
-            curl \
-            ca-certificates \
-            wget \
-            pwgen \
-            build-essential
+            apt-get install  -y curl ca-certificates wget pwgen build-essential
             echo "========================================================================"
             echo "基础软件包已全部安装完成！！"
             echo "========================================================================"
@@ -99,9 +87,16 @@ if [ ${UID} == 0 ]; then
         esac
         ;;
         2)
-        curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-        sudo apt-get install -y nodejs
+        apt-get update
+		apt-get install -y python-software-properties software-properties-common
+		add-apt-repository ppa:chris-lea/node.js
+		apt-get update
+		apt-get install nodejs
+		apt install nodejs-legacy
+		apt install npm
+		npm install -g cnpm --registry=https://registry.npm.taobao.org  
         node -v
+		cnpm -v
         echo "========================================================================"
         echo "Nodejs安装完成！！"
         echo "========================================================================"
@@ -138,49 +133,7 @@ if [ ${UID} == 0 ]; then
         echo "========================================================================"
     	;;
     	6)
-        DEBIAN_FRONTEND=noninteractive apt-get -yq install \
-            apache2 \
-            libapache2-mod-php5 \
-            php5-mysql \
-            php5-mcrypt \
-            php5-gd \
-            php5-curl \
-            php-pear \
-            php-apc
-        curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-        /usr/sbin/php5enmod mcrypt
-        echo "========================================================================"
-        echo "PHP安装完成！！"
-        echo "========================================================================"
-        echo "ServerName localhost" >> /etc/apache2/apache2.conf
-        sed -i "s/variables_order.*/variables_order = \"EGPCS\"/g" /etc/php5/apache2/php.ini
-        mkdir -p /workspace/php && rm -fr /var/www/html && ln -s /workspace/php /var/www/html
-        chown www-data:www-data /workspace/php -R
-        sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
-        a2enmod rewrite
-        source /etc/apache2/envvars
-        tail -F /var/log/apache2/* &
-        service apache2 start
-        echo "========================================================================"
-        echo "PHP安装完成！！Apache已经启动，public目录为workspace/php！！"
-        echo "启动Apache命令：service apache2 start"
-        echo "========================================================================"
-    	;;
-    	7)
-    	TOMCAT_MAJOR_VERSION="8"
-        TOMCAT_MINOR_VERSION="8.0.11"
-        CATALINA_HOME="/tomcat"
-        echo "下载中，稍等。"
-    	wget -q https://archive.apache.org/dist/tomcat/tomcat-${TOMCAT_MAJOR_VERSION}/v${TOMCAT_MINOR_VERSION}/bin/apache-tomcat-${TOMCAT_MINOR_VERSION}.tar.gz && \
-        wget -qO- https://archive.apache.org/dist/tomcat/tomcat-${TOMCAT_MAJOR_VERSION}/v${TOMCAT_MINOR_VERSION}/bin/apache-tomcat-${TOMCAT_MINOR_VERSION}.tar.gz.md5 | md5sum -c - && \
-        tar zxf apache-tomcat-*.tar.gz && \
-        rm apache-tomcat-*.tar.gz && \
-        mv apache-tomcat* $CATALINA_HOME
-        chmod +x $(pwd)/tomcat/*.sh && \
-        cd $(pwd)/tomcat && ./run.sh
-    	;;
-    	8)
-        apt-get -yq install mysql-server-5.6 && \
+        apt-get -y install mysql-server-5.6 && \
         rm -rf /var/lib/apt/lists/* && \
         rm /etc/mysql/conf.d/mysqld_safe_syslog.cnf && \
         mv $(pwd)/mysql/my.cnf /etc/mysql/conf.d/my.cnf && \
